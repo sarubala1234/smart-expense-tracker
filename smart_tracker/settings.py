154958f0b2a5b627a7679a31007d2efe3036c1ce
Saru Bala <jdsarubala1234@gmail.com>
2025-11-10 09:42:18 +0530

    Fix BASE_DIR, ROOT_URLCONF, and ALLOWED_HOSTS for Render

--- smart_tracker/settings.py
+++ smart_tracker/settings.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "$34@=f#8w1$d242deii6+(xkn#on3y7b(v16(=7+x&yc+h57he)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://smart-expense-tracker-r7le.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -33,7 +34,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'smart_tracker.urls'
+
 
 TEMPLATES = [
     {
